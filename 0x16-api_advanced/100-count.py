71592aa262a94ac9dda6b45fe296423e41e277ce
Sourcery AI <>
2023-06-23 11:23:32 +0000

    'Refactored by Sourcery'

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -12,7 +12,7 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
         after (str): The parameter for the next page of the API results.
         count (int): The parameter of results matched thus far.
     """
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
@@ -49,6 +49,6 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
             print("")
             return
         instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
-        [print("{}: {}".format(k, v)) for k, v in instances]
+        [print(f"{k}: {v}") for k, v in instances]
     else:
         count_words(subreddit, word_list, instances, after, count)
