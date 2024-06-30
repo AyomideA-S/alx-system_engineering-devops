26fa9e64b6844db7de604f079fae7787b31bdb73
AyomideA-S <midesuperbest@gmail.com>
2024-06-30 23:15:35 +0100

    Still trying to install that package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,6 +5,16 @@ package { 'python3-pip':
   ensure => installed,
 }
 
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
 exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   path    => ['/usr/bin', '/usr/local/bin'],
